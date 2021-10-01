3be8eb66aeff8244ffaff41ed67f59af9157e294
Marcin Michalski <evulmastah@gmail.com>
2021-10-01 01:35:39 +0100

    Update cs-fixer to 3.x (#10)

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,16 +1,17 @@
 <?php
 
-return PhpCsFixer\Config::create()
+$config = new PhpCsFixer\Config();
+
+return $config
     ->setRules([
         '@PSR2' => true,
         'array_syntax' => ['syntax' => 'short'],
-        'binary_operator_spaces' => ['align_double_arrow' => false, 'align_equals' => false],
         'blank_line_after_opening_tag' => true,
-        'blank_line_before_return' => true,
+        'class_attributes_separation' => ['elements' => ['method' => 'one']],
         'cast_spaces' => true,
         'concat_space' => ['spacing' => 'none'],
         'declare_strict_types' => true,
-        'method_separation' => true,
+        'increment_style' => ['style' => 'pre'],
         'native_function_invocation' => ['include' => ['@all'], 'strict' => false],
         'no_blank_lines_after_class_opening' => true,
         'no_spaces_around_offset' => ['positions' => ['inside', 'outside']],
@@ -19,15 +20,14 @@ return PhpCsFixer\Config::create()
         'phpdoc_align' => true,
         'phpdoc_no_access' => true,
         'phpdoc_separation' => true,
-        'pre_increment' => true,
         'single_quote' => true,
         'trim_array_spaces' => true,
         'single_blank_line_before_namespace' => true
     ])
     ->setFinder(
         PhpCsFixer\Finder::create()
-            ->in(__DIR__ . '/src')
-            ->in(__DIR__ . '/tests')
+            ->in(__DIR__.'/src')
+            ->in(__DIR__.'/tests')
     )
     ->setRiskyAllowed(true)
     ->setUsingCache(false)
